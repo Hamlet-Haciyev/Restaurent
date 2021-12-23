966509f55e77b32020ecaee2a9dfce6f22c9f4b0
HamletHajiyev <hamletoh@code.edu.az>
2021-12-23 23:54:17 +0400

    Front to Back

---
+++ DirectList (Backend-Project)/DirectList (Backend-Project)/Controllers/Listing-FullWidthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DirectList__Backend_Project_.Controllers
+{
+    public class Listing_FullWidthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
